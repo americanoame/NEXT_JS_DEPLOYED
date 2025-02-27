2cb57b4c959aa7335b7591a18c06ac23722292e2
Andre Silva <andrenations@gmail.com>
2025-02-27 17:16:44 -0500

    Add generic convertToPlainObject function for serializing data

--- prostore/lib/utils.ts
+++ prostore/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+// The `<T>` is a generic. We can use this to specify the type of the value that we are passing in. 
+// In this case, `T` represents a placeholder for any type that the function might accept when it is called. 
+// It could be a `string`, `object`, `array`, or even a more complex type like a Prisma model.
+
+export function convertToPlainObject<T>(value: T): T {
+  return JSON.parse(JSON.stringify(value));
+}
