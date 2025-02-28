a7f0b17d0b32d6857028a4748ea3e76b3987be17
Andre Silva <andrenations@gmail.com>
2025-02-27 20:16:08 -0500

    added title

--- prostore/app/(root)/page.tsx
+++ prostore/app/(root)/page.tsx
@@ -11,6 +11,7 @@ const HomePage = async () => {
 
   return (
     <div className='space-y-8'>
+      <h2 className='h2-bold'>Latest Products</h2>
       <ProductList title='Newest Arrivals' data={latestProducts} 
       limit={4}
       />
