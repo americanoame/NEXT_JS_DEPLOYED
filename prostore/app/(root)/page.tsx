8027ae17b5b37fb840a00f64d36ac7c279547881
Andre Silva <andrenations@gmail.com>
2025-02-27 19:36:21 -0500

    cleaned the code

--- prostore/app/(root)/page.tsx
+++ prostore/app/(root)/page.tsx
@@ -11,7 +11,6 @@ const HomePage = async () => {
 
   return (
     <div className='space-y-8'>
-      <h2 className='h2-bold'>Latest Products</h2>
       <ProductList title='Newest Arrivals' data={latestProducts} 
       limit={4}
       />
