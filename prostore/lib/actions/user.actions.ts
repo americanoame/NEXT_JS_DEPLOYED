94ff2319cde272d0be884e1a952320aa8dcd000d
Andre Silva <andrenations@gmail.com>
2025-03-03 12:47:15 -0500

    Implement sign-in and sign-up logic with validation and password hashing
    Created signInWithCredentials to handle user sign-in with validation using Zod schema
    Implemented user sign-out with SignOutUser function
    Added signUp function for user registration with password hashing (bcrypt-ts-edge)
    Ensured validation checks for sign-in and sign-up forms with detailed error handling
    Used prisma to store user data in the database upon successful sign-up
    Automatically signs in the user after successful registration

---
+++ prostore/lib/actions/user.actions.ts
@@ -0,0 +1,83 @@
+'use server';
+
+import { signInFormSchema, signUpFormSchema } from '../validator';
+import { signIn, signOut } from '@/auth';
+import { isRedirectError } from 'next/dist/client/components/redirect-error';
+import { hashSync } from 'bcrypt-ts-edge';
+import { prisma } from '@/db/prisma';
+
+
+// Sign in the user with credentials
+export async function signInWithCredentials(prevState: unknown,
+  formData: FormData) {
+  try {
+    // Set user from form and validate it with Zod schema
+    const user = signInFormSchema.parse({
+      email: formData.get('email'),
+      password: formData.get('password'),
+    });
+
+    
+
+    await signIn('credentials', user);
+
+    
+
+    return { success: true, message: 'Signed in successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return { success: false, message: 'Invalid email or password' };
+  }
+}
+
+// Sign the user out
+export async function SignOutUser() {
+  await signOut();
+}
+
+export default SignOutUser;
+
+
+// sing up user 
+export async function signUp(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      confirmPassword: formData.get('confirmPassword'),
+      password: formData.get('password'),
+    });
+
+    // you have to make a plan password beffore it hashed
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: 'User registered successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return {
+      success: false,
+      message: 'User was not registered',
+    };
+  }
+}
